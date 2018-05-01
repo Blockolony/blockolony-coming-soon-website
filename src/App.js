0eba2ed550a72bced9f1abde05feafdba69c0d9f
Jack Leslie <jjdl@st-andrews.ac.uk>
2018-05-02 00:22:09 +0100

    Logo is now svg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './assets/logo.png';
+import logo from './assets/logo.svg';
 import './css/App.css';
 
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
