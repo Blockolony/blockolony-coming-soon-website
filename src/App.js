ee137c775e9acf3885bbd7805aa89c78f219136b
Jack Leslie <jjdl@st-andrews.ac.uk>
2018-05-10 18:03:18 +0100

    Added Slack

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import faInstagram from '@fortawesome/fontawesome-free-brands/faInstagram';
 import faFacebook from '@fortawesome/fontawesome-free-brands/faFacebook';
 import faMeetup from '@fortawesome/fontawesome-free-brands/faMeetup';
+import faSlack from '@fortawesome/fontawesome-free-brands/faSlack';
 import faMail from '@fortawesome/fontawesome-free-solid/faEnvelope';
 
 class App extends Component {
@@ -25,8 +26,9 @@ class App extends Component {
           <h1 className="blockolony-title">Blockolony</h1>
           <ul className="blockolony-nav list-inline">
             <li className="list-inline-item"><a href="https://www.facebook.com/blockolony/"><FontAwesomeIcon icon={faFacebook}/></a></li>
-            <li className="list-inline-item"><a href="https://instagram.com/blockolony"><FontAwesomeIcon icon={faInstagram}/></a></li>
+            <li className="list-inline-item"><a href="https://instagram.com/blockolony/"><FontAwesomeIcon icon={faInstagram}/></a></li>
             <li className="list-inline-item"><a href="https://www.meetup.com/members/253224515/"><FontAwesomeIcon icon={faMeetup}/></a></li>
+            <li className="list-inline-item"><a href="https://blockolony.slack.com/"><FontAwesomeIcon icon={faSlack}/></a></li>
             <li className="list-inline-item">&#8729;</li>
             <li className="list-inline-item"><a href="mailto:blockolony@outlook.com"><FontAwesomeIcon icon={faMail}/></a></li>
           </ul>
